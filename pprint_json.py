f2e4f86630642337c90b99f4ad8e785f09324e21
AntonB <barhan@gkomega.ru>
2017-10-02 01:32:21 +0300

    Task completed

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,25 @@
 import json
+import os
 
+
+# загружаем файл, проверяем, что он существует.
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return print("Ошибка чтения файла. Проверьте его наличие по заданному пути")
+    else:
+        with open(filepath, 'r', encoding='UTF-8') as work_file:
+            return json.load(work_file)
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pass
+    file_path = input("Введите имя и путь до файла:")
+    json_file_content = load_data(file_path)
+    if json_file_content:
+        pretty_print_json(json_file_content)
+    else:
+        print('Данные не загруженны. Попробуйте еще раз')
